9250d11b29518232cc77a2e0391e7034cd79bc47
dhimanparas20 <dhimanparas20@gmail.com>
2024-03-30 22:37:44 +0530

    Nuke unnecessary modules
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- api/views.py
+++ api/views.py
@@ -1,18 +1,13 @@
-from .models import User
 from .serializers import *
 from django.contrib.auth import authenticate
-from django.forms.models import model_to_dict
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view,action
-from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet,GenericViewSet,ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import get_object_or_404
-from statistics import mean
 from django.utils import timezone
 
 #when sending token in header we nedd to specify Toekn or Bearer
